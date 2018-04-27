a7675bb0397e3045da137b3785fd428a02ba5f0e
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 09:55:40 +0000

    automatically backed up by learn

--- db/migrate/008_add_catchphrase_to_characters.rb
+++ db/migrate/008_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
-class AddCatchphraseToShows < ActiveRecord::Migration[4.2]
+class AddCatchphraseToCharacters < ActiveRecord::Migration[4.2]
   def change
-    add_column :shows, :catchphrase, :string
+    add_column :characters, :catchphrase, :string
   end
 end
