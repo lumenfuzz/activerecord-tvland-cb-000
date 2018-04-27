ccc39f67aee52f751c22ead2a527ac046b244ca6
Patrick J Louis <lumenfuzz@gmail.com>
2018-04-27 09:43:13 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,12 @@ class Actor < ActiveRecord::Base
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  def list_roles
+    roles = []
+    self.characters.each do |character|
+      roles << "#{character.name} - #{character.show.name}"
+    end
+  end
+  
 end
